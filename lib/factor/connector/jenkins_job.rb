5e98e854db5fb733663c9d2fa1c26b692c5e3da4
Maciej Skierkowski <maciej@skierkowski.com>
2014-10-21 00:05:26 -0700

    Adding a ton of methods

--- lib/factor/connector/jenkins_job.rb
+++ lib/factor/connector/jenkins_job.rb
@@ -8,6 +8,7 @@ Factor::Connector.service 'jenkins_job' do
     password = params['password']
     host     = params['host']
     filter   = params['filter']
+    status   = params['status']
 
 
     fail 'Host (host) is requires' unless host
@@ -19,9 +20,104 @@ Factor::Connector.service 'jenkins_job' do
 
     client = JenkinsApi::Client.new connection_settings
 
-    jobs = filter ? client.job.list(filter) : client.job.list_all
+    jobs = if filter
+      client.job.list_details(filter)
+    elsif status
+      client.job.list_all_with_details
+    else
+      client.job.list_all_with_details
+    end
 
     action_callback jobs
+  end
+
+  action 'build' do |params|
+    username = params['username']
+    password = params['password']
+    host     = params['host']
+    id       = params['job']
+
+    fail 'Host (host) is required' unless host
+    fail 'Job ID (job) is required' unless id
+
+    connection_settings = { server_ip: host }
+    connection_settings[:username] = username if username
+    connection_settings[:password] = password if password
+
+    client = JenkinsApi::Client.new connection_settings
+
+    code = client.job.build(id)
+
+    fail "Unable to build job: #{id}" unless code == '201'
+
+    action_callback code: code
+  end
+
+  action 'stop' do |params|
+    username = params['username']
+    password = params['password']
+    host     = params['host']
+    id       = params['job']
+    build    = params['build'] || 0
+
+    fail 'Host (host) is required' unless host
+    fail 'Job ID (job) is required' unless id
+
+    connection_settings = { server_ip: host }
+    connection_settings[:username] = username if username
+    connection_settings[:password] = password if password
+
+    client = JenkinsApi::Client.new connection_settings
+
+    code = client.job.stop_build(id, build)
+
+    fail "Unable to stop job: #{id}" unless code == '302'
+
+    action_callback code: code
+  end
+
+  action 'status' do |params|
+    username = params['username']
+    password = params['password']
+    host     = params['host']
+    id       = params['job']
+
+    fail 'Host (host) is required' unless host
+    fail 'Job ID (job) is required' unless id
+
+    connection_settings = { server_ip: host }
+    connection_settings[:username] = username if username
+    connection_settings[:password] = password if password
+
+    client = JenkinsApi::Client.new connection_settings
+
+    status = client.job.get_current_build_status(id)
+
+    action_callback status:status
+  end
+
+  action 'rename' do |params|
+    username = params['username']
+    password = params['password']
+    host     = params['host']
+    id       = params['job']
+    new_id   = params['new_job']
+
+    fail 'Host (host) is required' unless host
+    fail 'Job ID (job) is required' unless id
+    fail 'New Job ID (new_id) is required' unless new_id
+
+    connection_settings = { server_ip: host }
+    connection_settings[:username] = username if username
+    connection_settings[:password] = password if password
+
+    client = JenkinsApi::Client.new connection_settings
+
+    code = client.job.rename(id, new_id)
+
+    fail "Unable to rename job '#{id} to #{new_id}'" unless code == '302'
+
+    action_callback code: code
   end
 
 end
--- spec/jenkins_job_spec.rb
+++ spec/jenkins_job_spec.rb
@@ -2,18 +2,63 @@ require 'spec_helper'
 
 describe 'Jenkins' do
   describe 'Job' do
+    before do
+      @host = ENV['JENKINS_HOST']
+      @build = ENV['JENKINS_TEST_BUILD']
+      @service_instance = service_instance('jenkins_job')
+      @params = { 'host' => @host, 'job' => @build }
+    end
+
     it 'can list jobs' do
+      @service_instance.test_action('list',@params) do
+        puts expect_return
+      end
+    end
 
-      host = ENV['JENKINS_HOST']
+    it 'can build a job' do
+      @service_instance.test_action('build',@params) do
+        expect_return code: '201'
+      end
+    end
 
-      service_instance = service_instance('jenkins_job')
+    it 'can get current build status' do
 
-      params = {
-        'host' => host
-      }
+      @service_instance.test_action('status',@params) do
+        response = expect_return
 
-      service_instance.test_action('list',params) do
-        expect_return
+        expect(response[:payload]).to include(:status)
+      end      
+    end
+
+    it 'can get rename' do
+      params = @params.dup
+      params['new_job'] = 'factor-build-new'
+
+      @service_instance.test_action('rename',params) do
+        response = expect_return
+        expect(response[:payload]).to include(:code)
+        expect(response[:payload][:code]).to be == '302'
+      end
+
+      params['job'] = 'factor-build-new'
+      params['new_job'] = 'factor-build'
+      
+      @service_instance.test_action('rename',params) do
+        response = expect_return
+        expect(response[:payload]).to include(:code)
+        expect(response[:payload][:code]).to be == '302'
+      end
+    end
+
+    it 'can stop a build' do
+      @service_instance.test_action('build',@params) do
+        expect_return code: '201'
+      end
+
+      sleep 10
+
+      @service_instance.test_action('stop',@params) do
+        expect_return code: '302'
       end
     end
   end
